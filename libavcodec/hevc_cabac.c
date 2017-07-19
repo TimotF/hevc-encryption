77d3493509d99480d3ddd704a60d8790c9314a98
Timothe FRIGNAC <timothe.frignac@yahoo.fr>
2017-07-19 10:46:20 +0200

    Reencode cu_qp_delta_abs

--- libavcodec/hevc_cabac.c
+++ libavcodec/hevc_cabac.c
@@ -859,22 +859,45 @@ int ff_hevc_cu_qp_delta_abs(HEVCContext *s)
     int prefix_val = 0;
     int suffix_val = 0;
     int inc = 0;
-
-    while (prefix_val < 5 && GET_CABAC(elem_offset[CU_QP_DELTA] + inc)) {
-        prefix_val++;
-        inc = 1;
+#if HEVC_DECRYPT
+    HEVCLocalContext *lc = s->HEVClc;
+    cabac_data_t *const cabac = &lc->ccc;
+    cabac->cur_ctx = &(cabac->ctx.cu_qp_delta_abs[inc]);
+#endif
+    int bin = 1;
+    while (prefix_val < 5 && bin) {
+        bin = GET_CABAC(elem_offset[CU_QP_DELTA] + inc);
+#if HEVC_DECRYPT
+        CABAC_BIN(cabac, bin, "cu_qp_delta_abs");
+#endif
+        if(bin){
+            prefix_val++;
+            inc = 1;
+        }
     }
     if (prefix_val >= 5) {
         int k = 0;
-        while (k < CABAC_MAX_BIN && get_cabac_bypass(&s->HEVClc->cc)) {
-            suffix_val += 1 << k;
-            k++;
+        bin = 1;
+        while (k < CABAC_MAX_BIN && bin) {
+            bin = get_cabac_bypass(&s->HEVClc->cc);
+#if HEVC_DECRYPT
+            CABAC_BIN_EP(cabac, bin, "cu_qp_delta_abs");
+#endif
+            if(bin){
+                suffix_val += 1 << k;
+                k++;
+            }
         }
         if (k == CABAC_MAX_BIN)
             av_log(s->avctx, AV_LOG_ERROR, "CABAC_MAX_BIN : %d\n", k);
 
-        while (k--)
-            suffix_val += get_cabac_bypass(&s->HEVClc->cc) << k;
+        while (k--){
+            bin = get_cabac_bypass(&s->HEVClc->cc);
+#if HEVC_DECRYPT
+            CABAC_BIN_EP(cabac, bin, "cu_qp_delta_abs");
+#endif
+            suffix_val += bin << k;
+        }
     }
     return prefix_val + suffix_val;
 }
